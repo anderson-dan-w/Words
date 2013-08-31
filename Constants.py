887a7be8888c10277b022cfc1d1c3f5d6a04d12f
dwanderson <anderson.dan.w@gmail.com>
2013-08-31 00:15:27 -0400

    DOH, dont reference module within itself. Constants update

--- Constants.py
+++ Constants.py
@@ -8,6 +8,11 @@ py_text_dir = py_dir + "text/"
 TWL06_txt = py_text_dir + "TWL06.txt"
 WORDS_txt = py_text_dir + "WORDS.txt"
 
+words = set()
+for fname in (TWL06_txt, WORDS_txt):
+    _w = str(open(fname, "r").read()).replace("\r","").upper().split("\n")
+    words.update(_w)
+
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 primes = [ 2,  3,  5,  7, 11, 13, 17, 19, 23, 29, 31, 37,  41,
           43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]

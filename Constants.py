e9557593f5d778349d6b84fd6689a582bb8b260e
dwanderson <anderson.dan.w@gmail.com>
2013-08-30 23:43:14 -0400

    added primeabet to Constants

--- Constants.py
+++ Constants.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+# python modules
 import os
+import collections
 
 py_dir = os.path.realpath(os.path.dirname(__file__)) + "/"
 py_text_dir = py_dir + "text/"
@@ -7,3 +9,8 @@ TWL06_txt = py_text_dir + "TWL06.txt"
 WORDS_txt = py_text_dir + "WORDS.txt"
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+primes = [ 2,  3,  5,  7, 11, 13, 17, 19, 23, 29, 31, 37,  41,
+          43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+primeabet = collections.defaultdict(int)
+primeabet.update(zip(alphabet, primes))
+

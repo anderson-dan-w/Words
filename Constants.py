a233087c2cab222432762a6943addfe3bfb250f4
dwanderson <anderson.dan.w@gmail.com>
2013-08-30 20:58:28 -0400

    basic Constants file
    text filenames, and the alphabet; can't think of anything else that's very word-basic Constant just yet

---
+++ Constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import os
+
+py_dir = os.path.realpath(os.path.dirname(__file__)) + "/"
+py_text_dir = py_dir + "text/"
+TWL06_txt = py_text_dir + "TWL06.txt"
+WORDS_txt = py_text_dir + "WORDS.txt"
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"

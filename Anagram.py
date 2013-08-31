ec477a3172842a55f1bb4e8154f6d48f36e3171c
dwanderson <anderson.dan.w@gmail.com>
2013-08-31 00:26:23 -0400

    added _looping_anagram

--- Anagram.py
+++ Anagram.py
@@ -36,6 +36,41 @@ def _calc_value(word):
     return value
 
 
+def _looping_anagram(letters, howmany=1, MIN=3):
+    """ Find anagrams of a given set of letters. If only one anagram is desired
+        it is a look-up in a precomputed table. If more than one anagram is
+        requested, it iteratively considers every possible splitting of
+        letters, checking each for anagrams, that are at least MIN letters 
+        long, which defaults to 3.
+        Appropriate doctests under looping_anagram()
+    """
+    if howmany == 1:
+        return _anagrams[_calc_value(letters)]
+    already_seen = set()
+    anagrams = set()
+    len_letts = len(letters)
+    maxIters = 2 ** (len_letts - 1)
+    for itr in range(maxIters):
+        word1 = ""
+        word2 = ""
+        for pos in range(len_letts):
+            if (2**pos) & itr:
+                word1 += letters[pos]
+            else:
+                word2 += letters[pos]
+        if word1 in already_seen or word2 in already_seen:
+            continue
+        already_seen.update([word1, word2])
+        if len(word1) < MIN or len(word2) < (MIN * (howmany - 1)):
+            continue
+        anagram1 = _anagrams[_calcValue(word1)]
+        if not anagram1:
+            continue
+        anagram2 = _looping_anagram(word2, howmany-1)
+        for a1 in anagram1:
+            for a2 in anagram2:
+                anagrams.add(" ".join(sorted((a1 + " " + a2).split(" "))))
+    return anagrams
 
 ##############################################################################
 def not_main():

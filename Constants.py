5c2559b5c0a7322c75e92b8307a4cf2cf9425736
dwanderson <anderson.dan.w@gmail.com>
2013-09-22 18:28:58 -0400

    added scrabble scores to Constants

--- Constants.py
+++ Constants.py
@@ -19,3 +19,9 @@ primes = [ 2,  3,  5,  7, 11, 13, 17, 19, 23, 29, 31, 37,  41,
 primeabet = collections.defaultdict(int)
 primeabet.update(zip(alphabet, primes))
 
+scrabble_scores = collections.defaultdict(int)
+scrabble_scores.update(zip(alphabet, [1, 3, 3,  2, 1, 4, 2, 4, 1, 8, 5, 1,  3,
+                                     1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]))
+scrabble_distr = collections.defaultdict(int)
+scrabble_distr.update(zip(alphabet, [9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2,
+                                    6, 8, 2, 1,  6, 4, 6, 4, 2, 2, 1, 2, 1]))

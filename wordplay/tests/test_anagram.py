8d5c66f9a68962385bc7ad92ea408685547e9ace
dwanderson <anderson.dan.w@gmail.com>
2017-03-12 15:09:01 -0500

    consistent checks across functions, clearer

--- wordplay/tests/test_anagram.py
+++ wordplay/tests/test_anagram.py
@@ -4,10 +4,11 @@ from wordplay import anagram as A
 
 
 class TestAnagram(unittest.TestCase):
-    def test_basic_anagram(self):
+    def test_looping_anagram(self):
         letters = "AEGLLRY"
         expected = set(["ALLERGY", "GALLERY", "LARGELY", "REGALLY"])
-        self.assertEqual(expected, A.looping_anagram(letters))
+        observed = A.looping_anagram(letters)
+        self.assertEqual(expected, observed)
 
     def test_looping_anagram_two_words(self):
         letters = "BUGJINX"

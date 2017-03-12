75cc966f473147be875aa7b1a7cb06f364d0a566
dwanderson <anderson.dan.w@gmail.com>
2017-03-12 15:05:31 -0500

    add test for anagrams into two words

--- wordplay/tests/test_anagram.py
+++ wordplay/tests/test_anagram.py
@@ -8,3 +8,9 @@ class TestAnagram(unittest.TestCase):
         letters = "AEGLLRY"
         expected = set(["ALLERGY", "GALLERY", "LARGELY", "REGALLY"])
         self.assertEqual(expected, A.looping_anagram(letters))
+
+    def test_looping_anagram_two_words(self):
+        letters = "BUGJINX"
+        expected = set(["BUG JINX"])
+        observed = A.looping_anagram(letters, 2)
+        self.assertEqual(expected, observed)

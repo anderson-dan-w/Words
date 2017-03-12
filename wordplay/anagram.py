75a4d2b129aef0f14f284b4069340e37e80d684f
dwanderson <anderson.dan.w@gmail.com>
2017-03-12 16:07:06 -0500

    cleaning up anagram.py to be module, not main-able

--- wordplay/anagram.py
+++ wordplay/anagram.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
 from __future__ import print_function, division, absolute_import
 import collections
 import os
 import sys
 import operator
-from optparse import OptionParser
 
 from wordplay import constants
 
@@ -18,20 +16,28 @@ _textdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "texts")
 _texts = [os.path.join(_textdir, f) for f in os.listdir(_textdir)]
 
 
-def readin_words():
-    for fname in [f for f in _texts if f.endswith(".dict")]:
-        with open(fname) as fh:
-            text = str(fh.read()).replace("\r", "\n")
-            words = (w for w in text.upper().split("\n") if w)
-            WORDS.update(words)
-
-
 def _calc_value(letters):
     """ Calculate a unique value for a given string, which is presumed to be
         comprised of only upper-case letters. Violating this fails silently,
         giving the caller 0 in return.
     """
     return reduce(operator.mul, (constants.PRIMEABET[l] for l in letters), 1)
+
+
+def precalculate_values():
+    for word in WORDS:
+        value = _calc_value(word)
+        ANAGRAMS[value].add(word)
+        LEN_VALUES[len(word)][value].add(word)
+
+
+def readin_words():
+    for fname in [f for f in _texts if f.endswith(".dict")]:
+        with open(fname) as fh:
+            text = str(fh.read()).replace("\r", "\n")
+        words = (w for w in text.upper().split("\n") if w)
+        WORDS.update(words)
+    precalculate_values()
 
 
 def looping_anagram(letters, nwords=1, MIN=3):
@@ -147,75 +153,5 @@ def panvowellic(plus_y=False, only_once=True):
     return anagrams
 
 
-##############################################################################
-def not_main():
+if __name__ != '__main__':
     readin_words()
-    for word in WORDS:
-        value = _calc_value(word)
-        ANAGRAMS[value].add(word)
-        LEN_VALUES[len(word)][value].add(word)
-
-
-def main(args):
-    USAGE = "{} <letters> [-n=nwords] [--MIN=minsize]".format(sys.argv[0])
-    DESCR = "Returns list of anagrams for given letters and options"
-    p = OptionParser(usage=USAGE, description=DESCR)
-    p.add_option('-n', '--nwords', type='int', default=1,
-            help='Number of words to comprise a solution, default=1')
-    p.add_option('--MIN', type='int', default=3,
-            help='Minimum number of letters in each anagram, default=3')
-    p.add_option('-e', '--extra', type='int', default=0,
-            help="number of extra, 'variable' letters to use, default=0")
-    p.add_option('-s', '--start', type='str', default='A',
-            help="Letter to start <--extra> letters at, default='A'")
-    p.add_option('-c', '--condensed', action='store_true',
-            help='Prints condensed results in the <--extra> case')
-    options, args = p.parse_args(args)
-
-    ## error-check input
-    if len(args) != 1:
-        p.error("Requires 1 positional argument, <letters>")
-    letters = args[0].upper()
-    nwords, MIN = options.nwords, options.MIN
-    extra, start, condensed = options.extra, options.start, options.condensed
-    if isinstance(start, str):
-        start = constants.ALPHABET.index(start)
-    if nwords * MIN > len(letters):
-        p.error("nwords * MIN can't be greater than legnth of letters...")
-    if any(val < 0 for val in (nwords, MIN, extra, start)):
-        p.error("negative values for arguments make no physical sense")
-
-    ## read in only the necessary words
-    upper_limit = len(letters) + extra - (MIN * (nwords - 1))
-    readin_words()
-    for word in WORDS:
-        if nwords == 1 and len(word) != (len(letters) + extra):
-            continue
-        elif nwords > 1 and len(word) not in range(MIN, upper_limit + 1):
-            continue
-        value = _calc_value(word)
-        ANAGRAMS[value].add(word)
-        LEN_VALUES[len(word)][value].add(word)
-    
-    if extra == 0:
-        anagrams = looping_anagram(letters, nwords, MIN)
-        print(anagrams)
-    else:
-        anagrams = plus_many(letters, extra, nwords, MIN, start)
-        if condensed:
-            anagram_set = set()
-            for s in anagrams.values():
-                anagram_set.update(s)
-            print(anagram_set)
-        else:
-            print(anagrams)
-
-    plural = '' if len(anagrams) == 1 else 's'
-    print("Found {} answer{}".format(len(anagrams), plural))
-    return
-
-##############################################################################
-if __name__ == '__main__':
-    main(sys.argv[1:])
-else:
-    not_main()

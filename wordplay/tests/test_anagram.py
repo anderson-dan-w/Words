bf30f275b47fed144c3aa85d81a720433a0fe038
dwanderson <anderson.dan.w@gmail.com>
2017-03-12 15:02:46 -0500

    pep8 syntax changes in test_anagram

--- wordplay/tests/test_anagram.py
+++ wordplay/tests/test_anagram.py
@@ -1,9 +1,10 @@
 import unittest
 
-from wordplay.anagram import *
+from wordplay import anagram as A
+
 
 class TestAnagram(unittest.TestCase):
     def test_basic_anagram(self):
         letters = "AEGLLRY"
         expected = set(["ALLERGY", "GALLERY", "LARGELY", "REGALLY"])
-        self.assertEqual(expected, looping_anagram(letters))
+        self.assertEqual(expected, A.looping_anagram(letters))
